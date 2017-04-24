f3d7f4a9cb192b5fa8d868cd3c24d2684d6ee2d4
JCGrant <jamescolin.grant@gmail.com>
2017-04-24 21:25:43 +0100

    Add program which interprets a given jlang file

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import System.Environment
+import System.IO
+import Frontend.Lexer
+import Frontend.Parser
+import Backend.Interpreter
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  args <- getArgs
+  case args of
+    []    -> error "Usage: jlang file"
+    [arg] -> do
+      handle <- openFile arg ReadMode
+      contents <- hGetContents handle
+      let tokens = scanTokens contents
+      let ast = parse tokens
+      interpret ast
+    _     -> error "Too many arguments!"
